020fc13532de547b23cba91f71c35a38c616bfe8
Addair40 <1719110378@utectulancingo.edu.mx>
2020-11-23 20:27:08 +0000

    funcionando

---
+++ productos/db/db_delete.php
@@ -0,0 +1,10 @@
+ 
+ <?php
+    $id_persona = $_GET["id_persona"];
+
+    $db = new SQLite3("../../agenda.db");
+
+    $db->exec("DELETE FROM personas where id_persona='$id_persona';");
+
+    header("Location: ../index.php");
+?>
